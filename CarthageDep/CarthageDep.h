7eb504eab1a9c74f2e0dbb448e17e95e40512bf1
Cihan Cimen <CCimen@walmartlabs.com>
2018-11-13 10:12:24 -0800

    Initial Commit

---
+++ CarthageDep/CarthageDep.h
@@ -0,0 +1,19 @@
+//
+//  CarthageDep.h
+//  CarthageDep
+//
+//  Created by Cihan Cimen on 11/13/18.
+//  Copyright © 2018 Walmartlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarthageDep.
+FOUNDATION_EXPORT double CarthageDepVersionNumber;
+
+//! Project version string for CarthageDep.
+FOUNDATION_EXPORT const unsigned char CarthageDepVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageDep/PublicHeader.h>
+
+
