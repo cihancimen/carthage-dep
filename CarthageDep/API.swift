a247984b35c763b9efa9757daed08e0334a28f19
Cihan Cimen <CCimen@walmartlabs.com>
2018-11-13 10:33:02 -0800

    Static framework with a public API

---
+++ CarthageDep/API.swift
@@ -0,0 +1,14 @@
+//
+//  API.swift
+//  CarthageDep
+//
+//  Created by Cihan Cimen on 11/13/18.
+//  Copyright © 2018 Walmartlabs. All rights reserved.
+//
+
+import Foundation
+
+public struct Constants {
+    let message = "Test"
+}
+
